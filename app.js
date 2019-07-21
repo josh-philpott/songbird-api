c4261c3cdc6bd103949f67f0ec9d3cf33d48a539
Joshua Philpott <jphil529@gmail.com>
2019-07-21 15:09:13 -0600

    cleanup a cors mess

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const cors = require('cors')
-const dotenv = require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
@@ -19,12 +17,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(
-  cors({
-    origin: 'https://songbridge.netlify.com',
-    credentials: true
-  })
-)
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', indexRouter)
