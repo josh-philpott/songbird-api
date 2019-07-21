778ce113cbe7eb1225da7fdcd389e8c93b810710
Joshua Philpott <jphil529@gmail.com>
2019-07-21 14:02:50 -0600

    debug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
@@ -14,11 +15,11 @@ const app = express()
 
 app.listen(3002)
 
-app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', indexRouter)
