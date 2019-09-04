013236493ae4821775c45ef81fd7b513a8ab1b53
Joshua Philpott <jphil529@gmail.com>
2019-09-04 08:55:12 -0600

    sow message and update detection

--- services/broadcast.services.js
+++ services/broadcast.services.js
@@ -4,10 +4,13 @@ const { find, flatMap } = require('lodash')
 //TODO: Need to come up with a legitimate storage mechanism for broadcasts
 const currentBroadcasts = {}
 
-const create = async (broadcasterName, profileImageUrl, debug, socketId) => {
-  const broadcastId = debug
-    ? 'debugid'
-    : await generate('0123456789abcdefghijklmnopqrstuvwxyz', 6)
+const create = async (
+  spotifyUserId,
+  broadcasterName,
+  profileImageUrl,
+  socketId
+) => {
+  const broadcastId = spotifyUserId
 
   currentBroadcasts[broadcastId] = {
     broadcastId,
@@ -28,10 +31,60 @@ const list = () => {
   return Object.keys(currentBroadcasts)
 }
 
+/**
+ *
+ * @param {*} prev spotify currently playing object from the last update
+ * @param {*} current
+ */
+const shouldUpdateListeners = (prev, prevUpdateTime, current) => {
+  if (!prev) {
+    return true
+  }
+
+  /// if the song id's the same, is_playing is the same, and the song is about where we expect it to be, don't update
+  const isSameSongId = prev.id === current.id
+  const isSameIsPlaying = prev.is_playing === current.is_playing
+
+  const songTimeDiff = current.progress_ms - prev.progress_ms
+  const realTimeDiff = new Date().getTime() - prevUpdateTime.getTime()
+
+  const songSkipDetected = Math.abs(songTimeDiff - realTimeDiff) > 1000
+
+  if (isSameSongId && isSameIsPlaying && !songSkipDetected) {
+    return false
+  }
+
+  //default to returning true unless any of the above cases
+  return true
+}
+
 const update = (broadcastId, currentlyPlaying) => {
+  //determine if we need to update listeners
+  let prev, prevUpdateTime
+  if (currentBroadcasts[broadcastId]) {
+    prev = currentBroadcasts[broadcastId].currentlyPlaying
+    prevUpdateTime = currentBroadcasts[broadcastId].lastUpdated
+  }
+  const updateListeners = shouldUpdateListeners(
+    prev,
+    prevUpdateTime,
+    currentlyPlaying
+  )
+
   currentBroadcasts[broadcastId] = {
     ...currentBroadcasts[broadcastId],
     currentlyPlaying,
+    status: 'live',
+    lastUpdated: new Date()
+  }
+
+  return updateListeners
+}
+
+const handleBroadcasterDisconnect = broadcastId => {
+  currentBroadcasts[broadcastId] = {
+    ...currentBroadcasts[broadcastId],
+    status: 'offline',
     lastUpdated: new Date()
   }
 }
@@ -47,5 +100,6 @@ module.exports = {
   get,
   list,
   update,
-  getBySocketId
+  getBySocketId,
+  handleBroadcasterDisconnect
 }
